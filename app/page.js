980cced3667d8b19e96a76f92cc78c2c302aef8c
andrewm <andrewmillercode@gmail.com>
2024-10-12 11:02:50 -0700

    update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,20 @@ import "./globals.css";
 export default function Home() {
   return (
     <main>
-      
+      <section>
+        <h1>Track your calories with just a picture,</h1>
+       <Image alt={'tasty food' }>
+      </section>
+      <section>
+        <h1>Track your calories with just a picture,</h1>
+      </section>
+      <section>
+        <h1>Track your calories with just a picture,</h1>
+      </section>
+      <section>
+        <h1>Track your calories with just a picture,</h1>
+      </section>
+
     </main>
   );
 }
