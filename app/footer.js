31b4305e309512eca8f5354428e75fad91e49d04
andrewm <andrewmillercode@gmail.com>
2024-10-12 11:50:07 -0700

    ok

--- app/footer.js
+++ app/footer.js
@@ -5,10 +5,10 @@ import { useRouter } from 'next/navigation';
 export default function Footer() {
 
     const router = useRouter();
-
+    //https://teentechsf.org/
     return (
       <footer>
-        <button className='footerBtn'>Made with ❤️ for TechTeenSF</button>
+        <button onClick={()=>{window.location.href='https://teentechsf.org/'}} className='footerBtn'>Made with ❤️ for TechTeenSF</button>
         <button onClick={()=>{window.location.href='https://github.com/simonxuuu/calorie-ai'}}className='footerBtn'>Github Repo</button>
       </footer>
     );
