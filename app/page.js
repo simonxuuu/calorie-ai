315eb5e6d157b417a6d2d79172e2a781c62a4fa6
andrewm <andrewmillercode@gmail.com>
2024-10-12 11:31:56 -0700

    ok

--- app/page.js
+++ app/page.js
@@ -3,23 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section style={{marginTop:'10em'}}> <div>
-      <h1 className="heading">Track your calories, with one click.</h1>
-      <h2 className="subHeading">Subheading  Subheading Subheading Subheading Subheading Subheading</h2>
-      </div>
-       
-       <Image className='showcaseImage'width={500} height={500}alt='food' src={'/images/food1.jpg'}/>
+      <section style={{ marginTop: "10em" }}>
+        {" "}
+        <div style={{ marginRight: "0.1em" }}>
+          <h1 className="heading">Track your calories, with one click.</h1>
+          <h2 className="subHeading">
+            Subheading Subheading Subheading Subheading Subheading Subheading
+          </h2>
+          <button className="generalButton">Get Started</button>
+        </div>
+        <Image
+          className="showcaseImage"
+          width={500}
+          height={500}
+          alt="food"
+          src={"/images/food1.jpg"}
+        />
       </section>
-      <section>
-        <h1 className="heading">Track your calories with just a picture,</h1>
-      </section>
-      <section>
-        <h1>Track your calories with just a picture,</h1>
-      </section>
-      <section>
-        <h1>Track your calories with just a picture,</h1>
-      </section>
-
+      
     </main>
   );
 }
