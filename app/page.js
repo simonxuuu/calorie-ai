ff1308f3007eb6f1d20440233b61fb59bd2125a3
andrewm <andrewmillercode@gmail.com>
2024-10-12 11:14:00 -0700

    update

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
 import "./globals.css";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section>
-        <h1>Track your calories with just a picture,</h1>
-       <Image alt={'tasty food' }>
+      <section style={{marginTop:'10em'}}> <div>
+      <h1 className="heading">Track your calories with just a picture,</h1>
+      <h2 className="subHeading">Track your calories with just a picture,</h2>
+      </div>
+       
+       <Image className='showcaseImage'width={500} height={500}alt='food' src={'/images/food1.jpg'}/>
       </section>
       <section>
-        <h1>Track your calories with just a picture,</h1>
+        <h1 className="heading">Track your calories with just a picture,</h1>
       </section>
       <section>
         <h1>Track your calories with just a picture,</h1>
