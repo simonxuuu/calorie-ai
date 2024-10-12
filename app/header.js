95f8b09696431a095f50bddb3cd8121e9511b35b
andrewm <andrewmillercode@gmail.com>
2024-10-12 11:59:14 -0700

    logo metadata

--- app/header.js
+++ app/header.js
@@ -1,13 +1,24 @@
 'use client'
 import { useRouter } from 'next/navigation';
-
+import Image from 'next/image';
 export default function Header() {
 
     const router = useRouter();
 
     return (
       <header>
-        <button id='headerTitle' onClick={()=>{router.push('/');}}>Calorie AI</button>
+        <div style={{display: 'flex',
+  alignItems: 'center',
+  gap:'0.3em'}}>
+        <button id='headerTitle' onClick={()=>{router.push('/');}}>Calorie AI </button>
+        <Image
+          className="logo"
+          width={30}
+          height={30}
+          alt="food"
+          src={"/favicon.ico"}
+        />
+        </div>
         <div style={{gap:'1.5em',display:'flex'}}>
         <button onClick={()=>{document.getElementById("landing0").scrollIntoView({block: "center"}); }}className='headerBtn'>Why us</button>
         <button onClick={()=>{document.getElementById("landing1").scrollIntoView({block: "center"}); }}className='headerBtn'>Test</button>
