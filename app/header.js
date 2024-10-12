8b478785f265b9cf339c173bdd621df94e200094
andrewm <andrewmillercode@gmail.com>
2024-10-12 10:56:58 -0700

    basic header

--- app/header.js
+++ app/header.js
@@ -1,7 +1,13 @@
 export default function Header() {
     return (
       <header>
-        <h1>Calorie AI</h1>
+        <button>Calorie AI</button>
+        <div style={{gap:'1.5em',display:'flex'}}>
+        <button className='headerBtn'>Test</button>
+        <button className='headerBtn'>Test</button>
+        <button className='headerBtn'>Test</button>
+        </div>
+        <button className='loginBtn'>Login</button>
       </header>
     );
   }
