238097c29fd7886753186ae2e5d1d1fcaa27dc3b
simonxuuu <simonxuuu@gmail.com>
2025-01-04 01:02:42 +0000

    Disclaimeer

--- app/footer.js
+++ app/footer.js
@@ -8,6 +8,7 @@ export default function Footer() {
     //https://teentechsf.org/
     return (
       <footer>
+        <span>This website uses predictive algorithms - it is not 100% accurate.</span>
         <button onClick={()=>{window.location.href='https://teentechsf.org/'}} className='footerBtn'>Made with ❤️ for TechTeenSF</button>
         <button onClick={()=>{window.location.href='https://github.com/simonxuuu/calorie-ai'}}className='footerBtn'>Github Repo</button>
       </footer>
