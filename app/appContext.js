da23dec0bb90769a00db8cc695c8c05aa345a1cc
simon <simonxuuu@gmail.com>
2025-01-26 21:06:33 -0800

    Clean

--- app/appContext.js
+++ app/appContext.js
@@ -7,48 +7,7 @@ const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [uid, setUid] = useState(null);
-  const [isAuthButton, setIsAuthButton] = useState(false);
-
-  useEffect(() => {
-    const session = supabase.auth.session();
-    if (session) {
-      setLoggedIn(true);
-      setUid(session.user.id);
-    } else {
-      console.log("Not logged in.");
-      setLoggedIn(false);
-    }
-
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-      if (session) {
-        setLoggedIn(true);
-        setUid(session.user.id);
-      } else {
-        setLoggedIn(false);
-      }
-    });
-
-    return () => {
-      authListener.unsubscribe();
-    };
-  }, []);
-
-  async function loginAccountWithGoogle() {
-    if (isAuthButton) return;
-    setIsAuthButton(true);
-    const { user, error } = await supabase.auth.signIn({
-      provider: 'google',
-    });
-    if (error) {
-      console.error('Error during sign-in: ', error);
-      setIsAuthButton(false);
-      return "Error logging in with Google.";
-    } else {
-      setIsAuthButton(false);
-      return "Success!";
-    }
-  }
+  const [userName, setUserName] = useState(null);
 
   return (
     <AppContext.Provider value={{ loggedIn, loginAccountWithGoogle }}>
--- app/supabaseClient.js
+++ app/supabaseClient.js
@@ -1,8 +1,12 @@
-// app/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 
-export default supabase;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase environment variables')
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
