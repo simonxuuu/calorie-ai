18bf54ae2448522e300658250ed930de6daac170
simon <simonxuuu@gmail.com>
2025-01-29 13:19:52 -0800

    Added middleware

--- app/utils/middleware.js
+++ app/utils/middleware.js
@@ -1,5 +1,12 @@
 import { NextResponse, NextRequest } from 'next/server'
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 
-export async function middleware(jwt) {
+export async function middleware(req) {
+    const res = NextResponse.next();   
+    const supabase = createMiddlewareClient({ req, res });
+    if (!user) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
     
+    return res;
 }
